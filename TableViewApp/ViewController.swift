5aa7323f9c48ab7204a497bfb017e00815222224
Anton <verbiants@gmail.com>
2021-04-14 19:38:32 +0300

    Initial Commit

---
+++ TableViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by Антон Вербицкий on 14.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
